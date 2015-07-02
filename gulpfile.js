ac5ec1c53737ccd456eaae72d1f26476c4b17214
Alexey Protasov <a.protasov@corp.mail.ru>
2015-07-02 17:14:37 +0300

    Add SASS support. Add more tasks. Optimize workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,75 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp');
 var del = require('del');
 var babelify = require('babelify');
 var browserify = require('browserify');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var eslint = require('gulp-eslint');
+var scsslint = require('gulp-scss-lint');
+var minifycss = require('gulp-minify-css');
+var cleanhtml = require('gulp-cleanhtml');
 var runSequence = require('run-sequence');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
-gulp.task('js', function() {
+gulp.task('css:dev', function() {
+  return gulp.src([ './scss/all.scss' ])
+    .pipe(sass())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('css:prod', function() {
+  return gulp.src([ './scss/all.scss' ])
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('html', function() {
+  return gulp.src([ './index.html' ])
+    .pipe(cleanhtml())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js:dev', function() {
   return browserify({ entries: [ './js/index.js' ] })
     .transform(babelify)
     .bundle()
     .pipe(source('all.js'))
     .pipe(buffer())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('js:prod', function() {
+  return browserify({ entries: [ './js/index.js' ] })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('all.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'))
+    .on('error', function(err) {
+      console.log('Error : ' + err.message);
+    });
+});
+
+gulp.task('scss-lint', function() {
+  return gulp.src([ './scss/*.scss' ])
+    .pipe(scsslint())
+    .pipe(scsslint.failReporter());
+});
+
+gulp.task('es-lint', function() {
+  return gulp.src([ 'js/**/*.js' ])
     .pipe(eslint())
-    .pipe(gulp.dest('./js'));
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
 });
 
 gulp.task('watch', function() {
-  return gulp.watch([
-    '!./js/all.js', // Exclude compiled file
-    './js/**/*.js'
-  ], [ 'js' ]);
-});
-
-gulp.task('copy', function() {
-  return gulp.src([ 'js/all.js' ], { base: './' }).pipe(gulp.dest('./dist'));
+  return gulp.watch([ './js/**/*.js' ], [ 'css:dev', 'js:dev' ]);
 });
 
 gulp.task('clean', function(callback) {
@@ -36,8 +77,8 @@ gulp.task('clean', function(callback) {
 });
 
 gulp.task('build', function() {
-  return runSequence('compile', 'clean', 'copy');
+  return runSequence('clean', /*'scss-lint', */'es-lint', 'compile');
 });
 
-gulp.task('compile', [ 'js' ]);
+gulp.task('compile', [ 'css:prod', 'js:prod', 'html' ]);
 gulp.task('default', [ 'watch' ]);
